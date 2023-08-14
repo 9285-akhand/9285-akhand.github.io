6f6a99c2f06b08823f41ca31b5b7bbb22751bef8
gitAV <akhandverma01062003@gmail.com>
2023-08-14 17:46:43 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Akhand Verma",
+  title: "Web Designer & Developer",
+  email: "akhandverma01062003@gmail.com",
+  gitHub: "9285-akhand",
+  linkedIn: "akhand-verma-2551b2229",
+  instagram: "akhand_5015",
+  twitter: "AkhandV18",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
